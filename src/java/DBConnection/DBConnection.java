e4840b99c0fb28ecf75d0eb1f2989844e37129a6
RAJASEKARAN <RAJASEKARAN@DESKTOP-GEGCV71>
2026-01-22 18:30:23 +0530

    update

--- src/java/DBConnection/DBConnection.java
+++ src/java/DBConnection/DBConnection.java
@@ -1,12 +1,9 @@
 package DBConnection;
 
-import java.sql.*;
-import java.util.Properties;
-import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection {
-
-    // Change this flag to switch between local XAMPP and online DB
     private static final boolean USE_ONLINE_DB = true;
 
     public static Connection getConnection() throws Exception {
@@ -16,13 +13,11 @@ public class DBConnection {
         String username;
         String password;
 
-        if(USE_ONLINE_DB) {
-            // Online DB (freesqldatabase.com)
-            url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12814967";
+        if (USE_ONLINE_DB) {
+            url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12814967?useSSL=false&allowPublicKeyRetrieval=true";
             username = "sql12814967";
             password = "HMF8FUaECZ";
         } else {
-            // Localhost DB (XAMPP)
             url = "jdbc:mysql://localhost:3306/svs";
             username = "root";
             password = "";
