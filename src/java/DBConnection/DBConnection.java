7d196b0c79a4c1c4798a667f11f67de2f7c7aa02
RAJASEKARAN <RAJASEKARAN@DESKTOP-GEGCV71>
2026-02-28 15:18:27 +0530

    update

--- src/java/DBConnection/DBConnection.java
+++ src/java/DBConnection/DBConnection.java
@@ -8,37 +8,31 @@ public class DBConnection {
     private static final boolean USE_ONLINE_DB = true;
 
     public static Connection getConnection() throws Exception {
+
         String url;
         String username;
         String password;
 
         if (USE_ONLINE_DB) {
-            // PostgreSQL driver
+
             Class.forName("org.postgresql.Driver");
 
-            // Use your database name here (likely "postgres")
-            url = "jdbc:postgresql://db.zoaafspbjvqzbxovoyrd.supabase.co:5432/postgres?sslmode=require";
-            username = "postgres";
-            password = "kfQKrgLTL7SjEcST";
+            // 🔥 GET VALUES FROM RENDER ENVIRONMENT
+            url = System.getenv("DB_URL");
+            username = System.getenv("DB_USER");
+            password = System.getenv("DB_PASSWORD");
 
         } else {
-            // Local MySQL
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
             url = "jdbc:mysql://localhost:3306/svs";
             username = "root";
             password = "";
         }
 
         Connection con = DriverManager.getConnection(url, username, password);
-        System.out.println("Connected successfully to Supabase!");
+        System.out.println("Connected successfully!");
         return con;
-    }
-
-    public static void main(String[] args) {
-        try {
-            Connection con = getConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
